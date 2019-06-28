0af775ab63b4dd0c1d1eb5f8c2e741787055022d
MaciejSawicki <dabster07@gmail.com>
2019-06-28 18:09:58 +0200

    Create Converters.

---
+++ src/main/java/com/sawicki/spring5recipeapp/converters/IngredientCommandToIngredient.java
@@ -0,0 +1,35 @@
+package com.sawicki.spring5recipeapp.converters;
+
+import com.sawicki.spring5recipeapp.commands.IngredientCommand;
+import com.sawicki.spring5recipeapp.domain.Ingredient;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngredientCommandToIngredient implements Converter<IngredientCommand, Ingredient> {
+
+    private final UnitOfMeasureCommandToUnitOfMeasure uomConverter;
+
+    public IngredientCommandToIngredient(UnitOfMeasureCommandToUnitOfMeasure uomConverter) {
+        this.uomConverter = uomConverter;
+    }
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Ingredient convert(IngredientCommand source) {
+        if(source == null) {
+            return null;
+        }
+
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(source.getId());
+        ingredient.setAmount(source.getAmount());
+        ingredient.setDescription(source.getDescription());
+        ingredient.setUnitOfMeasure(uomConverter.convert(source.getUnitOfMeasure()));
+
+        return ingredient;
+    }
+}
