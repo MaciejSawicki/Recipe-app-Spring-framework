59fbd6104994b435c878df66ade2aa9082aecf31
MaciejSawicki <dabster07@gmail.com>
2019-06-28 18:08:43 +0200

    Recipe Service.

--- src/main/java/com/sawicki/spring5recipeapp/services/RecipeService.java
+++ src/main/java/com/sawicki/spring5recipeapp/services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.sawicki.spring5recipeapp.services;
 
+import com.sawicki.spring5recipeapp.commands.RecipeCommand;
 import com.sawicki.spring5recipeapp.domain.Recipe;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +12,6 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
 
     Recipe findById(long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
