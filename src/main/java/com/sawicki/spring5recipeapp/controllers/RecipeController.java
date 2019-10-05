79b9a92f9d3c199b19fd65c4f9ebaed1ecbbc359
MaciejSawicki <dabster07@gmail.com>
2019-10-05 14:13:35 +0200

    Exception handling - Number format exception

--- src/main/java/com/sawicki/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/sawicki/spring5recipeapp/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.sawicki.spring5recipeapp.controllers;
 
 import com.sawicki.spring5recipeapp.commands.RecipeCommand;
+import com.sawicki.spring5recipeapp.exceptions.NotFoundException;
 import com.sawicki.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Slf4j
@@ -57,5 +57,35 @@ public class RecipeController {
         recipeService.deleteById(id);
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+
+        log.error("handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormatException(Exception exception) {
+
+        log.error("handling number format exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("numberformatexception");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
     }
 }
